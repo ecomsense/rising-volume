73023a385d3b8a0360e03c599fa35d6fb5aedf4a
prog@ecomsense.in <installer@gmail.com>
2025-01-23 01:28:11 +0530

    add updated settings

--- src/x_static/positions.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  tblColCnt(tableId="table", colNumber=7, ttlTagName="pnl" );
-  //tblColCnt(tableId="table", colNumber=5, ttlTagName="netqty" );
-});
-
